4ac0b9123371095c86382a943b7f0dc84e1086fc
Jesse Nazario <jessenzr@gmail.com>
2015-07-17 08:30:39 -0300

    more packages

---
+++ unity-mod/hjkl-alttab.sh
@@ -0,0 +1,4 @@
+sed -i "s/\(case NUX_VK_UP:\)/\1 case NUX_VK_K: case NUX_VK_k:/" launcher/SwitcherView.cpp
+sed -i "s/\(case NUX_VK_DOWN:\)/\1 case NUX_VK_J: case NUX_VK_j:/" launcher/SwitcherView.cpp
+sed -i "s/\(case NUX_VK_LEFT:\)/\1 case NUX_VK_H: case NUX_VK_h:/" launcher/SwitcherView.cpp
+sed -i "s/\(case NUX_VK_RIGHT:\)/\1 case NUX_VK_L: case NUX_VK_l:/" launcher/SwitcherView.cpp

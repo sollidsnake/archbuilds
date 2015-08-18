b8123b79b27a3f18af80a863a34c93435eafed59
Jesse Nazario <jessenzr@gmail.com>
2015-08-18 08:42:50 -0300

    firefox ubuntu

---
+++ firefox-ubuntu/sed-menu.sh
@@ -0,0 +1,3 @@
+sed -i "s/\(fileMenu.accesskey.*\)\"F\"/\1\"I\"/" browser/locales/en-US/chrome/browser/browser.dtd
+sed -i "s/\(bookmarksMenu.accesskey.*\)\"B\"/\1\"O\"/" browser/locales/en-US/chrome/browser/browser.dtd
+
